180ace040b99a65f32959e08836fb295e7d06b82
root <root@atlantico04208.virgo.atlantico.net.br>
2023-08-28 17:39:42 -0300

    FEAT | Added 'myLibs.js' to hold javascript functions

---
+++ src/myLibs.js
@@ -0,0 +1,28 @@
+function makeFetch({ endpoint, method, resourceId }) {
+    const realUrl = '';
+    const baseURL = 'http://localhost:7000/';
+    baseURL = baseURL.concat(endpoint);
+    
+    const headers = {"Content-Type": "application/json"};
+    const params = {
+        method: method,
+        headers: headers
+    };
+    
+    if (resourceId.length > 0) {
+        // realUrl = baseURL.concat(`/${resourceId}`)
+        realUrl = baseURL.concat('/', `/${resourceId}`)
+    }
+    else{
+        realUrl = baseURL
+    }
+
+    fetch(realUrl, params)
+    .then((resp) => resp.json())
+    .then((data) => {
+        return data
+    })
+    .catch((err) => console.log(err));
+}
+
+export default makeFetch;
